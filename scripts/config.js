2595bcb7c1a4bd04b78079055f18e45e77ad7485
Repeat <zhengbiwen@datagrand.com>
2025-02-13 11:20:36 +0800

    update

--- scripts/config.js
+++ scripts/config.js
@@ -3,6 +3,6 @@ export const config = {
   password: 'R7czTnCHYmpoe36x',
   loginUrl: 'https://kms-dev.datagrand.com/admin/#/kms-login',
   pageUrl: 'https://kms-dev.datagrand.com/#/knowledge/lib/overview',
-  loginUrl: 'http://127.0.0.1:5002/admin/#/kms-login',
-  pageUrl: 'http://127.0.0.1:5002/#/knowledge/lib/overview',
+  // loginUrl: 'http://127.0.0.1:5002/admin/#/kms-login',
+  // pageUrl: 'http://127.0.0.1:5002/#/knowledge/lib/overview',
 };
